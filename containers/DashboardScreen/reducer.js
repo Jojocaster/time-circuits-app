14b57ac1fdb267806a788528123fad6cac37235a
Joel Beaudon <joel.beaudon@gmail.com>
2018-06-18 22:13:21 +0100

    row reducer fixed

--- containers/DashboardScreen/reducer.js
+++ containers/DashboardScreen/reducer.js
@@ -1,25 +1,32 @@
-import { GREEN, RED, YELLOW, OUT_OF_SYNC } from '../../helpers/constants';
+import {
+  GREEN,
+  RED,
+  YELLOW,
+  OUT_OF_SYNC,
+  IN_SYNC
+} from '../../helpers/constants';
 import { EDIT_DISPLAY } from '../TimeRow/actions';
 
 const defaultState = [
-  { id: 0, label: 'last time departed', color: GREEN, sync: OUT_OF_SYNC },
+  { id: 0, label: 'last time departed', color: GREEN, sync: IN_SYNC },
   { id: 1, label: 'present time', color: YELLOW, sync: OUT_OF_SYNC },
   { id: 2, label: 'destination time', color: RED, sync: OUT_OF_SYNC }
 ];
 
 export const rows = (state = defaultState, action) => {
-  switch (action) {
+  switch (action.type) {
     case EDIT_DISPLAY:
-      const { rId } = action.row;
+      const { rId } = action.display;
 
-      return Object.assign({}, state, {
-        rows: state.rows.map((row) => {
-          if (row.id === rId) {
-            return Object.assign({}, row, {
-              sync: OUT_OF_SYNC
-            });
-          }
-        })
+      return state.map((row) => {
+        if (row.id === rId) {
+          return {
+            ...row,
+            sync: OUT_OF_SYNC
+          };
+        }
+
+        return row;
       });
     default:
       return state;
